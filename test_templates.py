4163f9b73395da45cec2cc1278dfa54e4e4f9492
Roman1R <635roman.r642@gmail.com>
2024-04-10 21:24:09 +1000

    template template reduced to human form

--- test_templates.py
+++ test_templates.py
@@ -4,7 +4,7 @@ import os
 app = Flask(__name__)
 
 
-@app.route('/index')
+@app.route('/index')  # EDITING...
 @app.route('/')
 def site():
     params = {
@@ -12,33 +12,40 @@ def site():
     return render_template("index_ads.html", **params)
 
 
-@app.route("/form_register", methods=['GET', 'POST'])
+@app.route("/form_register", methods=['GET', 'POST'])  # NEED A TEMPLATE, REALIZATION
 def form_register():
     params = {
     }
     return render_template("form_register.html", **params)
 
 
-@app.route("/form_ad", methods=['GET', 'POST'])
+@app.route("/form_ad", methods=['GET', 'POST'])  # NEED A REALIZATION
 def form_ad():
     params = {
     }
     return render_template("form_ad.html", **params)
 
 
-@app.route("/catmail")
+@app.route("/rating")  # NEED A TEMPLATE, REALIZATION
+def rating():
+    params = {
+    }
+    return render_template("rating.html", **params)
+
+
+@app.route("/catmail")  # NEED A TEMPLATE, REALIZATION
 def catmail():
     params = {
     }
     return render_template("catmail.html", **params)
 
 
-@app.route("/catdocs")
+@app.route("/catdocs")  # NEED A DOCS
 def catdocs():
     return render_template("catdocs.html")
 
 
-@app.route("/donate")
+@app.route("/donate")  # finished
 def donate():
     return render_template("donate.html")
 
