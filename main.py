3fb18031bbb5630e9e88b784664c488150e34407
Romromius <118234421+Romromius@users.noreply.github.com>
2024-04-25 21:46:33 +1000

    release?

--- main.py
+++ main.py
@@ -214,4 +214,4 @@ def check_out():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, port=port, host='127.0.0.1')
+    app.run(debug=False, port=port, host='0.0.0.0')
